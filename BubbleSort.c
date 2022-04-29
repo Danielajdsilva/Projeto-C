dcfc869faa5875f5ac49b7ce139e04eb65dfeedd
Danielajdsilva <47960120+Danielajdsilva@users.noreply.github.com>
2022-04-29 19:50:09 -0300

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -3,17 +3,6 @@
 #include<time.h>
 
 
-/*
-*   O BUBBLE SORT pode ser considerado o mais simples entre os algoritmos de comparação;
-*   Complexidade O(n²) ele é executado com a lógica de n-1.
-*   Este algoritmo é considerado instavel pois mantem a ordem dos iguais.
-*/
-
-/*
-*   Iniciamos o algorítmo recebendo ou criando uma lista, tanto pelo próprio código ou um arquivo a ser ordenado.
-*   Nesse caso a lista é criada na função "main" e executada a ordenação dentro da função "bubbleSort".
-*/
-
 int main(){
     srand (time(NULL));
 
@@ -31,23 +20,6 @@ int main(){
     }
 
 }
-
-    /*   
-    *   
-    *   O primeiro laço de repetição é um contador que conta de 0 até n-1,
-    *   que vai do primeiro até o penultimo número.
-    *   No laço for interno realiza um procedimento que faz referência das posições dentro da lista.
-    *   Na condição if compara então um elemento com o elemento ao lado. 
-    *   Se o primeiro é maior que o segundo realiza a troca. Se não, repete o laço e verifica-se os numeros seguintes.
-    *   
-    *   No inicio declaramos uma variável para saber se houve ou não uma troca.
-    *   Se hover a troca a lista será executada novamente pelo contador.
-    *   O segundo if verifica se houve ou não uma troca, se houve troca o laço do contador continua.
-    *   Caso de não haver trocas o laço será quebrado, sem precisar de mais repetições.
-    * 
-    *   Melhor caso é haver 0 trocas, pois o laço de repetição apenas irá verificar a lista uma vez e quebrará o laço do contador.
-    *   No pior caso o laço contador repetirá n vezes e o laço interno também verificará n vezes.
-    */
 
 int bubbleSort(int lista[], int n){
     int trocado;
