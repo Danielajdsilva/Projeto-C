25d2926d16b022ff05898d276f25580a944390de
Danielajdsilva <47960120+Danielajdsilva@users.noreply.github.com>
2022-04-29 19:51:45 -0300

    Update MergeSort.c

--- MergeSort.c
+++ MergeSort.c
@@ -6,11 +6,6 @@
 void merge(int lista[], int inicio, int meio, int fim){
   int i, j, k;
 
-  /***
-   * Cria a variável L = lista[p..q] e R ← lista[q+1..r]
-   * Criando assim uma matriz de dois números(conjunto) para serem ordenados
-   * 
-  */ 
 
   int n1 = meio - inicio + 1;
   int n2 = fim - meio;
@@ -21,22 +16,10 @@ void merge(int lista[], int inicio, int meio, int fim){
     R[j] = lista[meio + 1+ j];
 
 
-    /*
-     * Logo em seguida mantém o índice atual do conjunto formado pela função acima
-     * e da matriz principal designada pelo k que será formada após as comparações.
-     * 
-     * 
-    */
-
-    i = 0; //topo de i
-    j = 0; // topo de j
+    i = 0; 
+    j = 0; 
     k = inicio;
 
-    /*
-     * Até chegar ao final de L ou R,
-     * O while escolhe o maior entre os elementos L e R e coloca na posição correta na lista
-     * O "i" e "j" significam tambem o topo de suas listas.
-    */
     while (i < n1 && j < n2){
         if (L[i] <= R[j]){
             lista[k] = L[i];
@@ -48,9 +31,7 @@ void merge(int lista[], int inicio, int meio, int fim){
     k++;
     }
 
-    /*
-     * Quando ficarmos sem elementos em L ou R, esse while pega os elementos restantes e coloca na lista
-    */ 
+
     while (i < n1){
         lista[k] = L[i];
         i++;
@@ -66,19 +47,7 @@ void merge(int lista[], int inicio, int meio, int fim){
     }
 }
 
-/*
- * Nesse bloco o Merge faz a separação dos blocos inicio, fim e meio
- * dividimos o vetor em duas partes
- * se o inicio for menor que o final
- * o fim sempre inicia com o vetor inteiro, pois o parametro passado no main 
- * para que possamos fazer a divisão
- * então como o fim sempre inicia com o maior valor, fazemos uma divisão do vetor por 2
- * assim passando os paramêtros para a função merge
- * 
- * A função mergeSort divide repetidamente o array em duas metades
- * até chegarmos a um estágio em que tentamos executar MergeSort em um caso de tamanho 1
- * 
-*/
+
 void mergeSort(int lista[], int inicio, int fim){
     if (inicio < fim){
         int meio = (fim+inicio)/2;
