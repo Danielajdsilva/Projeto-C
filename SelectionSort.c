8555a4504e0cf7aeff693626b9be91a46946cbc3
Danielajdsilva <47960120+Danielajdsilva@users.noreply.github.com>
2022-04-29 19:52:21 -0300

    Update SelectionSort.c

--- SelectionSort.c
+++ SelectionSort.c
@@ -2,10 +2,6 @@
 #include<stdlib.h>
 #include<time.h>
 
-/*
-*   Iniciamos o algorítmo recebendo ou criando uma lista, tanto pelo próprio código ou um arquivo a ser ordenado.
-*   Nesse caso a lista é criada na função "main" e executada a ordenação dentro da função "bubbleSort".
-*/
 
 int main(){
     srand (time(NULL));
@@ -17,16 +13,7 @@ int main(){
         lista[i] = rand() % 10;
     }
 
-    /****
-     * No início, assumimos que o menor elemento está no índice 0.
-     * iniciamos a partir do segundo índice comparando os elementos.
-     * Na primeira execução ele encontrará o menor valor da lista.
-     * após encontrado ele será setado como a posição [0]
-     * e após essa execução ele não percorrerá mais o item [0] da lista
-     * e ordenará o menor número depois do anterior encontrado e assim por diante.
-     * 
-     * O if somente será executado caso o lista[menor] não seja maior que qualquer outra posição da lista
-     */
+
 
     int menor, aux;
     for(int i = 0; i < n-1; i++){
